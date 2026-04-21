c2056ae83a0485029d134201bc29ceb3c8e72244
RiiikkiTikkiTavi <RiiikkiTikkiTavi@users.noreply.github.com>
2026-04-21 12:19:24 +0300

    исправлена ошибка в AppRouter

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,4 +1,7 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flash/features/cards/view/card_list_screen.dart';
+import 'package:flash/features/learning/view/learning_screen.dart';
+import 'package:flash/features/sets/view/set_list_screen.dart';
 
 part 'router.gr.dart';
 
@@ -6,8 +9,8 @@ part 'router.gr.dart';
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
-    AutoRoute(page: SetListScreen.page, initial: true),
-    AutoRoute(page: CardListScreen.page),
-    AutoRoute(page: LearningScreen.page),
+    AutoRoute(page: SetListRoute.page, initial: true),
+    AutoRoute(page: CardListRoute.page),
+    AutoRoute(page: LearningRoute.page),
   ];
 }
