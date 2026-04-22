cc02f5e11295f98ae43158fddb1700baaf087328
RiiikkiTikkiTavi <RiiikkiTikkiTavi@users.noreply.github.com>
2026-04-22 18:59:19 +0300

    создание вспомогательного класса инициализации objectbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flash/app/flash_app.dart';
+import 'package:flash/object_box.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Инициализация базы данных
+  final objectbox = await ObjectBox.init();
+
   runApp(const ProviderScope(child: FlashApp()));
 }
---
+++ lib/object_box.dart
@@ -0,0 +1,45 @@
+import 'package:flash/data/models/card_model.dart';
+import 'package:flash/data/models/progress_model.dart';
+import 'package:flash/data/models/set_model.dart';
+import 'package:flash/objectbox.g.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+
+// Точка входа для использования ObjectBox.
+// Это прямой интерфейс к базе данных,
+// который управляет объектами Box
+
+class ObjectBox {
+  // главный объект ObjectBox для получения доступа к боксам
+  late final Store store;
+
+  // коробки для хранения данных
+  late final Box<CardModel> cardBox;
+  late final Box<ProgressModel> progressBox;
+  late final Box<SetModel> setBox;
+
+  // запрещаем создание объекта извне с помощью приватного конструктора
+  ObjectBox._();
+
+  // создание экземпляра ObjectBox
+  static Future<ObjectBox> init() async {
+    // создание пустого объекта через приватный конструктор
+    final obj = ObjectBox._();
+
+    // получение пути к папке
+    final dir = await getApplicationDocumentsDirectory();
+    final dbDir = path.join(dir.path, 'flashcards');
+
+    // открытие БД в папке dbDir
+    obj.store = openStore(directory: dbDir);
+
+    // получение боксов для хранения моделей
+    obj.cardBox = obj.store.box<CardModel>();
+    obj.progressBox = obj.store.box<ProgressModel>();
+    obj.setBox = obj.store.box<SetModel>();
+
+    return obj;
+  }
+
+  void close() => store.close();
+}
